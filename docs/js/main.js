03839148c896473a4b13920dcdda23f7c33e4dd0
mstanka <mice78.mm@gmail.com>
2020-05-14 20:51:54 +0200

    toggle menu

---
+++ docs/js/main.js
@@ -0,0 +1,17 @@
+const mainNav = document.getElementById("js-menu");
+const navToggle = document.getElementById("js-navbar-toggle");
+const menuIcon = document.getElementById("js-menu-icon");
+
+navToggle.addEventListener("click", () => {
+  mainNav.classList.toggle("active");
+
+  if (mainNav.classList.contains("active")) {
+    menuIcon.removeAttribute("src", "img/menu-open.svg");
+    menuIcon.setAttribute("src", "img/menu-close.svg");
+    navToggle.style.right = "2rem";
+  } else {
+    menuIcon.removeAttribute("src", "img/menu-close.svg");
+    menuIcon.setAttribute("src", "img/menu-open.svg");
+    navToggle.style.right = "-3rem";
+  }
+});
